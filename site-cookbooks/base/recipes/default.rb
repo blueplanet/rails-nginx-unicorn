52ddb241415a7c657ad780038f66cf898a05f3a0
blueplanet <erguolinge@gmail.com>
2015-04-11 18:02:46 +0900

    Change node name; remove imagemagick

--- site-cookbooks/base/recipes/default.rb
+++ site-cookbooks/base/recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-%w(git gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel libffi-devel libxml2-devel libxslt-devel file file-devel mysql-devel ImageMagick ImageMagick-devel).each do |pkg|
+%w(git gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel libffi-devel libxml2-devel libxslt-devel file file-devel mysql-devel).each do |pkg|
   package pkg do
     action :install
   end
